a15687723c0b2762270abb5d772582e25375ced9
lapollazadelawXXL <158739419+lapollazadelawXXL@users.noreply.github.com>
2024-02-21 19:23:32 +0100

    C1

---
+++ src/Colaborador.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Colaborador {
+
+    private static ArrayList <Integer> edades = new ArrayList <>();
+
+    public Colaborador(){
+
+
+    }
+
+    public static ArrayList<Integer> getEdades() {
+        return edades;
+    }
+}
